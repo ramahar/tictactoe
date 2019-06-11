92210b5a3cb15ac5a7400c526e10a4454e83ef41
Rayyan <rmahar@ucsd.edu>
2019-06-11 00:15:46 -0700

    Add board

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,27 @@
-tictactoe
+// Contains all winning combinations 
+var winners = new Array();
+// Store selections in array 
+var player1Selections = new Array();
+var player2Selections = new Array();
+
+var currentPlayer = 0;  // keeps track of current player 
+var points1 = 0;    // player 1
+var points2 = 0;    // player 2 
+var size = 3;   // Size of playing board 
+
+function drawBoard() {
+  var parent = document.getElementById("game");
+  var counter = 1;
+
+  for (let i = 0; i < 3; i++) {
+    let row = document.createElement("tr");
+
+    for (let j = 0; j < size; j++) {
+      let col = document.createElement("td");
+      col.innerHTML = counter;
+
+      row.appendChild(col);
+    }
+    parent.appendChild(row);
+  }
+}
